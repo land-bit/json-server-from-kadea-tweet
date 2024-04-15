478ef628d5c41582a485c9e0a15d72174251a311
blessingtutka <blessingtutka298@gmail.com>
2024-04-15 10:45:44 +0200

    Set Json Server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+const jsonRouter = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+
+server.use(jsonRouter);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
+module.exports = server
